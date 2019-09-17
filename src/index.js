11f2f13f6fe69b1c9821cd6d4eb3cd0bd24f40eb
Denze1l <dvornikovd.o.1998@gmail.com>
2019-09-17 11:21:05 +0300

    start project add fonts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
+
 import classBoard from './js/class-board';
-// import PNotify from 'pnotify/dist/es/PNotify';
-// import 'pnotify/dist/es/PNotifyButtons';
-// import 'pnotify/dist/es/PNotifyStyleMaterial';
+import PNotify from 'pnotify/dist/es/PNotify';
+import 'pnotify/dist/es/PNotifyButtons';
+import 'pnotify/dist/es/PNotifyStyleMaterial';
 
 
-// import * as basicLightbox from 'basiclightbox';
-// import infiniteScroll from 'infinite-scroll';
+import * as basicLightbox from 'basiclightbox';
+import infiniteScroll from 'infinite-scroll';
 
-// import axios from 'axios';
+import axios from 'axios';
 
-//CSS
-// import 'material-design-icons/iconfont/material-icons.css';
-// import 'pnotify/dist/PNotifyBrightTheme.css';
-// import 'basiclightbox/dist/basicLightbox.min.css';
-// import './css/styles.css';
+CSS
+import "./css/fonts.css"
+import 'material-design-icons/iconfont/material-icons.css';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import './css/styles.css';
 
 const board = new classBoard();
 console.log(board.def);
